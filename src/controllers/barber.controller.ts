b645d51a6bc27d56af086e57eb7d9b429aece1c7
MARCO <jmukhammadsodikh@gmail.com>
2026-08-04 19:40:07 +0900

    feat: frontend related css, js and image creation

--- src/controllers/barber.controller.ts
+++ src/controllers/barber.controller.ts
@@ -10,7 +10,7 @@ const memberService = new MemberService();
 barberController.goHome = (req: Request, res: Response) => {
     try {
         console.log('goHome')
-        res.send('Home Page');
+        res.render("home");
         // send | json | redirect | end | render
     }
     catch (err) {
@@ -21,7 +21,7 @@ barberController.goHome = (req: Request, res: Response) => {
 barberController.getLogin = (req: Request, res: Response) => {
     try {
         console.log('getLogin')
-        res.send('Login Page');
+        res.render("login");
     }
     catch (err) {
         console.log("Error, getLogin:", err)
@@ -31,7 +31,7 @@ barberController.getLogin = (req: Request, res: Response) => {
 barberController.getSignup = (req: Request, res: Response) => {
     try {
         console.log('getSignup')
-        res.send('Signup Page');
+        res.render("signup");
     }
     catch (err) {
         console.log("Error, getSignup:", err)
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ dotenv.config();
 import mongoose from 'mongoose';
 import app from './app';
 
-mongoose.connect(process.env.MONGO_URL as string, {})
+mongoose.connect(process.env.MONGO_URL as string, {}) // Call
     .then((data) => {
         console.log("MongoDB connection succeed");
-        const PORT = process.env.PORT ?? 3009;
+        const PORT = process.env.PORT ?? 3008;
         app.listen(PORT, function () {
-            console.log(`The server is running successfully on port: ${PORT}`);
+            console.info(`The server is running successfully on port: ${PORT}`);
+            console.info(`Admin project on http://localhost:${PORT}/admin \n`)
         });
     })
     .catch((err) => console.log("ERROR on connection MongoDB", err));
