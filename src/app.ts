e3c90575f647b3a765d278f86e9649a7d227abe8
MARCO <jmukhammadsodikh@gmail.com>
2026-08-06 18:15:41 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,15 @@ import routerAdmin from "./routerAdmin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session"; // Md integration to our webserver
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session)
+const store = new MongoDBStore({
+    uri: String(process.env.MONGO_URL),
+    collection: 'sessions'
+});
+
 
 /** 1-ENTRANCE **/
 const app = express();
@@ -14,6 +23,18 @@ app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
 /** 2-SESSIONS **/
+app.use(
+    session({
+        secret: String(process.env.SESSION_SECRET),  // code for creating sessions.. secret.env
+        cookie: {
+            maxAge: 1000 * 3600 * 3, // session time 3h
+        },
+        store: store,   // mongodb store sessions
+
+        resave: true,  // 12:00 auth => 15:00, 15:00 auth = 18:00 ends
+        saveUninitialized: true
+    })
+)
 
 /** 3-VIEWS **/
 app.set('views', path.join(__dirname, 'views'));
